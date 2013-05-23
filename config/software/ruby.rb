55975348079712c80df4bfb04584edee89af051a
Lamont Granquist <lamont@opscode.com>
2013-05-23 15:50:57 -0700

    bump the plevel

--- config/software/ruby.rb
+++ config/software/ruby.rb
@@ -87,7 +87,7 @@ build do
 
   case platform
   when "aix"
-    patch :source => "ruby-aix-configure.patch", :plevel => 0
+    patch :source => "ruby-aix-configure.patch", :plevel => 1
     # --with-opt-dir causes ruby to send bogus commands to the AIX linker
   when "freebsd"
     configure_command << "--without-execinfo"
